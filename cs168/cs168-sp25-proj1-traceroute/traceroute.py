f6282306c02f64140ced8433d28b5359dc543eb5
Fatih <yildizahmetfatih@gmail.com>
2025-08-03 14:11:20 +0300

    cs 168: project 1a (traceroute) completed

--- cs168/cs168-sp25-proj1-traceroute/traceroute.py
+++ cs168/cs168-sp25-proj1-traceroute/traceroute.py
@@ -18,6 +18,9 @@ TRACEROUTE_PORT_NUMBER = 33434  # Cisco traceroute port number.
 # single router before giving up and moving on.
 PROBE_ATTEMPT_COUNT = 3
 
+def _bytes_to_bitstring(buffer: bytes) -> str:
+    return ''.join(format(byte, '08b') for byte in buffer)
+
 class IPv4:
     # Each member below is a field from the IPv4 packet header.  They are
     # listed below in the order they appear in the packet.  All fields should
@@ -38,7 +41,20 @@ class IPv4:
     dst: str
 
     def __init__(self, buffer: bytes):
-        pass  # TODO
+        bitstring = _bytes_to_bitstring(buffer)
+
+        self.version = int(bitstring[0:4], 2)           # 4 bits
+        self.header_len = int(bitstring[4:8], 2) * 4    # 4 bits
+        self.tos = int(bitstring[8:16], 2)              # 8 bits
+        self.length = int(bitstring[16:32], 2)          # 16 bits
+        self.id = int(bitstring[32:48], 2)              # 16 bits
+        self.flags = int(bitstring[48:51], 2)           # 3 bits
+        self.frag_offset = int(bitstring[51:64], 2)     # 13 bits
+        self.ttl = int(bitstring[64:72], 2)             # 8 bits
+        self.proto = int(bitstring[72:80], 2)           # 8 bits
+        self.cksum = int(bitstring[80:96], 2)           # 16 bits
+        self.src = str(int(bitstring[96:104], 2)) + "." + str(int(bitstring[104:112], 2)) + "." + str(int(bitstring[112:120], 2)) + "." + str(int(bitstring[120:128], 2))   # 32 bits
+        self.dst = str(int(bitstring[128:136], 2)) + "." + str(int(bitstring[136:144], 2)) + "." + str(int(bitstring[144:152], 2)) + "." + str(int(bitstring[152:160], 2))  # 32 bits
 
     def __str__(self) -> str:
         return f"IPv{self.version} (tos 0x{self.tos:x}, ttl {self.ttl}, " + \
@@ -60,7 +76,11 @@ class ICMP:
     cksum: int
 
     def __init__(self, buffer: bytes):
-        pass  # TODO
+        bitstring = _bytes_to_bitstring(buffer)
+
+        self.type = int(bitstring[0:8], 2)     # 8 bits                   
+        self.code = int(bitstring[8:16], 2)    # 8 bits
+        self.cksum = int(bitstring[16:32], 2)  # 16 bits
 
     def __str__(self) -> str:
         return f"ICMP (type {self.type}, code {self.code}, " + \
@@ -79,13 +99,17 @@ class UDP:
     cksum: int
 
     def __init__(self, buffer: bytes):
-        pass  # TODO
+        bitstring = _bytes_to_bitstring(buffer)
+
+        self.src_port = int(bitstring[0:16], 2)     # 16 bits                   
+        self.dst_port = int(bitstring[16:32], 2)    # 16 bits
+        self.len = int(bitstring[32:48], 2)         # 16 bits
+        self.cksum = int(bitstring[48:64], 2)       # 16 bits
 
     def __str__(self) -> str:
         return f"UDP (src_port {self.src_port}, dst_port {self.dst_port}, " + \
             f"len {self.len}, cksum 0x{self.cksum:x})"
 
-# TODO feel free to add helper functions if you'd like
 
 def traceroute(sendsock: util.Socket, recvsock: util.Socket, ip: str) \
         -> list[list[str]]:
@@ -107,10 +131,40 @@ def traceroute(sendsock: util.Socket, recvsock: util.Socket, ip: str) \
     should be included as the final element in the list.
     """
 
-    # TODO Add your implementation
-    for ttl in range(1, TRACEROUTE_MAX_TTL+1):
-        util.print_result([], ttl)
-    return []
+    routers: list[list[str]] = []
+    for ttl in range (1, TRACEROUTE_MAX_TTL + 1):
+        sendsock.set_ttl(ttl)
+
+        responders = set()
+        for _ in range(PROBE_ATTEMPT_COUNT):
+            sendsock.sendto("potato".encode(), (ip, TRACEROUTE_PORT_NUMBER))
+
+            if recvsock.recv_select():
+                buf, address = recvsock.recvfrom()
+
+                # print(buf.hex()) # *** for debugging, REMOVE later ***
+
+                outer_ipv4 = IPv4(buf[:20])
+                icmp = ICMP(buf[outer_ipv4.header_len:outer_ipv4.header_len + 8])
+
+                if icmp.type == 11 and icmp.code == 0: # Time Exceeded
+                    responders.add(outer_ipv4.src)
+                elif icmp.type == 3 and icmp.code == 3: # Destination Unreachable
+                    # finish up and return
+                    inner_ipv4_offset = outer_ipv4.header_len + 8
+                    inner_ipv4 = IPv4(buf[inner_ipv4_offset:inner_ipv4_offset + 20])
+                    destination_ip = inner_ipv4.dst
+
+                    if destination_ip != ip:
+                        continue
+
+                    util.print_result([destination_ip], ttl)
+                    routers.append([destination_ip])
+                    return routers
+
+        routers.append(list(responders))
+        util.print_result(list(responders), ttl)
+    return routers
 
 
 if __name__ == '__main__':
